c2ee0a0f571dd62238aae46f310267a029463097
Bhawana Dhamala <bhawanadhamala75@gmail.com>
2022-02-07 07:16:21 +0545

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let curDate = new Date( );
+curDate = curDate.getHours();
+let greeting = '';
+const cssStyle = {};
+
+if (curDate >= 1 && curDate < 12) {
+  greeting = "Good Morning";
+  cssStyle.color = 'rgb(137, 141, 133';
+} else if (curDate >= 12 && curDate < 19) {
+  greeting = "Good Afternoon";
+  cssStyle.color = 'rgb(207, 125, 58)';
+} else {
+  greeting = "Good Night";
+  cssStyle.color = 'black';
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <div>
+      <h1> 
+        Hello Bhawana, <span style={cssStyle}> { greeting } </span>
+      </h1>
+    </div>
+  </>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
